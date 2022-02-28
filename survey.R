ab99c43b5ba0f60157bc8962ec28219d5706de43
Damiano Oldoni <damiano.oldoni@inbo.be>
2022-02-28 17:29:42 +0100

    Improve assignment of technologies to assessors
    @timadriaens: run all and you should get a decent df. Please, let me know if you would like to have the results in a better way for copy pasting.
    Notice that you can set up another standard deviation limit (row 18). Of course lower it is, more balanced is the distribution of assessors per technologies, but also more computation time. For a standard deviation of 2 it takes about 10 seconds to find it.

--- survey.R
+++ survey.R
@@ -1,3 +1,7 @@
+#'  This code performs a random assignment of 10 technologies per assessor and
+#'  taking into account that each technology is evaluated by exactly 10
+#'  assessors
+
 library(readxl)
 library(dplyr)
 assessors <- read_excel("Data/tech_assessors.xlsx")
@@ -7,19 +11,57 @@ assessors_list
 tech_list <- na.omit(assessors$"Approaches/technologies")
 tech_list
 
-as.data.frame(table(sample(tech_list, 520, replace = TRUE)))
 
-list.ass <- vector("list", length(assessors_list))
-i = 1
-set.seed(4)
-while (i <= length(tech_list)) {
-  list.ass[[i]] <- sample(x = assessors_list, 10, replace = FALSE)
-  i = i + 1
+# n technologies per assessor
+n <- 10
+# max value allowed to st_deviation of n assessors per technology
+st_dev_limit <- 2
+
+st_dev <- 100 # initialize standard eviation
+verbose <- FALSE # if TRUE more details are returned on screen
+
+while (st_dev > st_dev_limit) {
+  # create matrix with n rows as number of assessors and ncols as number of
+  # technologies
+  assignments <- matrix(
+    rep(0, length(assessors_list)*length(tech_list)),
+    nrow = length(assessors_list), 
+    ncol = length(tech_list)
+  )
+  for (i in seq(assessors_list)) {
+    if (verbose) {
+      message(paste0("Assigning technologies to ",
+                   assessors_list[i],
+                   " (", i, "/", n, ")"))
+    }
+    technologies_sample <- sample(
+      x = seq(length(tech_list)),
+      size = n,
+      replace = FALSE
+    )
+    assignments[i, technologies_sample] <- 1
+  }
+  st_dev <- sd(colSums(assignments))
+  if (st_dev > st_dev_limit) {
+    message(paste0(
+      "Too high standard deviation: ", st_dev, " > ", st_dev_limit
+      )
+    )
+  } else {
+      message(paste0("Standard deviation (", st_dev, ")  is acceptable."))
+    }
 }
 
-class(list.ass[[1]])
-assignments_temp <- as.data.frame(do.call("rbind",list.ass))
-assignments_temp %>%
-  mutate(tecnologies = tech_list) -> assignments
+message("Technologies assigned to all assessors.")
+message(paste0("Standard deviation: ", st_dev))
 
-as.data.frame(table(unlist(list.ass)))
+colnames(assignments) <- tech_list
+
+assignments <- as_tibble(assignments)
+
+assessors <- assessors %>% select(Assessors)
+
+assignments <- assessors %>% bind_cols(assignments)
+
+assignments
+View(assignments)
